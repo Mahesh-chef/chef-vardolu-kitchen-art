29bf2c8f0f4675b3da5457c98b34fb5ba2ea0b6f
Mahesh-chef <mahesh.vardolu789@gmail.com>
2025-06-09 15:59:23 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/chef-portfolio/' : '/',
+  base: mode === 'production' ? '/chef-vardolu-kitchen-art/' : '/',
   server: {
     host: "::",
     port: 8080,
